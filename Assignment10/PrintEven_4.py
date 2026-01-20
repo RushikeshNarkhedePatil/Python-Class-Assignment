c101e54d0ddfee5e84f48934bcb4a44e0229edc8
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2026-01-20 22:10:44 +0530

    Print even number

---
+++ Assignment10/PrintEven_4.py
@@ -0,0 +1,20 @@
+# 4. Write a program which accepts one number and prints all even numbers till that
+# number.
+# Input: 10
+# Output: 2 4 6 8 10
+
+def PrintEven(no):
+    for i in range(2,no+1):
+        if(i % 2 == 0):
+            print(i,end=" ")
+
+    print()
+
+
+def main():
+    Number = int(input("Enter Number to print table : "))
+
+    PrintEven(Number)
+
+if __name__ == "__main__":
+    main()
