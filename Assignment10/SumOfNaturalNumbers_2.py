b73c345fe81c3eb5b61409128932c07ad8571151
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2026-01-20 21:49:59 +0530

    Sumesion of natural numbers

---
+++ Assignment10/SumOfNaturalNumbers_2.py
@@ -0,0 +1,20 @@
+# 2. Write a program which accepts one number and prints sum of rst N natural numbers.
+# Input: 5
+# Output: 15
+
+def PrintNaturalNumber(no):
+    iSum = 0
+    for i in range(1,no+1):
+        iSum =iSum +i
+    return iSum
+
+
+
+def main():
+    Number = int(input("Enter Number to print table : "))
+
+    Sum = PrintNaturalNumber(Number)
+    print("Summention of natural number is : ",Sum)
+
+if __name__ == "__main__":
+    main()
