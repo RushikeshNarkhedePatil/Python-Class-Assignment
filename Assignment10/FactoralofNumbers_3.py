0d2016ebacd63f62facf7dea40c9c2a74873ab14
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2026-01-20 22:03:20 +0530

    Factorial of number

---
+++ Assignment10/FactoralofNumbers_3.py
@@ -0,0 +1,19 @@
+# 3. Write a program which accepts one number and prints factorial of that number.
+# Input: 5
+# Output: 120
+
+def Factorial(no):
+    Fact = 1
+    for i in range(1,no+1):
+        Fact = Fact * i
+    return Fact
+
+
+def main():
+    Number = int(input("Enter Number to print table : "))
+
+    Fact = Factorial(Number)
+    print("Factorial of numbers is : ",Fact)
+
+if __name__ == "__main__":
+    main()
