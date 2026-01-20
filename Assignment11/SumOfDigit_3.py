03cdf84c2589f53b0d01917c6fab8252970413f9
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2026-01-20 23:33:42 +0530

    Sum of Digit

---
+++ Assignment11/SumOfDigit_3.py
@@ -0,0 +1,25 @@
+# 3. Write a program which accepts one number and prints sum of digits.
+# Input: 123
+# Output: 6
+
+def SumOfDigit(no):
+    Sum = 0
+    rem = 0
+    if(no == 0):
+        return 0
+    while(no > 0):
+        rem = no % 10
+        Sum = Sum + rem
+        no = no // 10
+    return Sum
+
+
+
+def main():
+    Number = int(input("Enter Number to print table : "))
+
+    Count = SumOfDigit(Number)
+    print(Count)
+
+if __name__ == "__main__":
+    main()
